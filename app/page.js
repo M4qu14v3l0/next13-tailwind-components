256ad369ea4d253bfa314339eff2ec54dcb9052f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-06-20 12:34:51 -0500

    adding commit

--- app/page.js
+++ app/page.js
@@ -10,29 +10,48 @@ const sections = [
   {value:1 , bg:'bg-green-100'},
   {value:2 , bg:'bg-yellow-100'},
   {value:3 , bg:'bg-blue-100'},
-  {value:4 , bg:'bg-red-100'},
-  {value:5 , bg:'bg-white'},
 ]
 
+const sections2 = [
+  {value:1 , bg:'bg-green-400'},
+  {value:2 , bg:'bg-yellow-400'},
+  {value:3 , bg:'bg-blue-400'},
+]
+
+
 export default function Home() {
+
   
+  const container = useRef(null)
   gsap.registerPlugin(ScrollTrigger)
- 
+  
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-    gsap.to(".box", {
-      scrollTrigger: {
-        trigger: ".container-box",
-        start: "top center",
-        scrub:1,
-        pin:true,
-        toggleActions: "restart pause reverse pause",
-        markers: true,
-      },
-      rotation:360,
+
+    const tl = gsap.timeline()
+      tl.from(".box", {
+      y:100,
       ease:"none",
-      duration:3
+      duration:2
     })
+    .from(".box2", {
+      y:100,
+      ease:"none",
+      duration:2
+    })
+
+    ScrollTrigger.create({
+      animation: tl,
+      trigger: ".container-box",
+      start: "top center",
+      end: "bottom 50%",
+      pin:true,
+      pinSpacing: false,
+      scrub:1,
+      toggleActions: "restart pause reverse pause",
+      markers: true,
+    })
+
     })
     return () => {
       ctx.revert()
@@ -43,7 +62,7 @@ export default function Home() {
 
   return (
     <main className="bg-black h-full text-white">
-      <div className='h-[100vh] mb-[400px] flex flex-col items-center justify-center'>
+      <div className='h-[500px] flex flex-row items-center justify-center'>
         <p>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,  comes from a line in section 1.10.32
         </p>
@@ -52,17 +71,24 @@ export default function Home() {
         </p>
 
       </div>
-      <div className='container-box'>
+      <div className='h-[500px] mb-[300px] flex flex-col items-center gap-5 container-box' ref={container}>
         {
           sections.map((item , index) => (
-            <div className={`h-10 w-full flex justify-center items-center box ${item.bg}`} key={index}>
+            <div className={`h-5 w-10 flex justify-center items-center box text-black ${item.bg}`} key={index}>
+              <p>{item.value}</p>
+            </div>
+          ))
+        }
+        {
+          sections2.map((item , index) => (
+            <div className={`h-5 w-10 flex justify-center items-center box2 text-black ${item.bg}`} key={index}>
               <p>{item.value}</p>
             </div>
           ))
         }
       </div>
 
-      <div className='h-[800px] flex flex-col items-center justify-center'>
+      <div className='h-[500px] flex flex-col items-center justify-center'>
         <p>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,  comes from a line in section 1.10.32
         </p>
