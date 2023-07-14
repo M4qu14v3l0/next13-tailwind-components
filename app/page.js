7298421e2c38108e0f7b00cd750a3f53a202eea4
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-07-13 21:19:54 -0500

    push

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Banner_card from "./designSystem/banner_card"
 //gsap
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useEffect , useRef } from "react";
+import { useEffect , useState } from "react";
 
 
 
@@ -17,16 +17,17 @@ export default function Home() {
 
   //plugin register
   gsap.registerPlugin(ScrollTrigger)
-
+  
   useEffect(() => {
     const ctx = gsap.context(() => {
+
       const elementsSelected = gsap.utils.toArray('.card-test')
-      console.log(elementsSelected)
       elementsSelected.forEach((card , index) => {
+
         const itemAppear = gsap.timeline({
           paused:true,
           reversed:true,
-          defaults: {ease:'ease'},
+          defaults: {ease:'ease-in-out'},
           scrollTrigger: {
             trigger: card,
             start: 'top center',
@@ -37,10 +38,28 @@ export default function Home() {
               endColor: 'yellow',
             }
           }
-        })
+        }) 
+
         itemAppear
-        .fromTo(card , {xPercent:-500} , {xPercent: 0})
+        .fromTo(card , {xPercent:-500 , } , {xPercent: 0 })
+
+        const hoverTimeline = gsap.timeline({ paused: true });
+        hoverTimeline.to(card, { scale: 1.2  });
+  
+        card.addEventListener('mouseenter', () => {
+          hoverTimeline.timeScale(1).play();
+        });
+  
+        card.addEventListener('mouseleave', () => {
+          hoverTimeline.timeScale(1.5).reverse();
+        });
+
+
       })
+
+      
+
+      
 
       ScrollTrigger.create({
         trigger: '.card-test-container',
@@ -58,16 +77,19 @@ export default function Home() {
     return () => {
       ScrollTrigger.killAll()
     }
-  })
+  } , )
+
 
   return (
     <main className="flex flex-col justify-center items-center">
       <section className="h-[1000px]">
       </section>
           <div className="h-[500px] flex flex-col gap-10 justify-center items-center card-test-container">
-          <div className="h-32 w-[800px] rounded-xl color-change-5x card-test">
+          <div className="h-32 w-[800px] rounded-xl color-change-5x card-test " >
           </div>
-          <div className="h-32 w-[800px] rounded-xl color-change-5x card-test">
+          <div className="h-32 w-[800px] rounded-xl color-change-5x card-test " >
+          </div>
+          <div className="h-32 w-[800px] rounded-xl color-change-5x card-test " >
           </div>
       </div>
       <section className="h-[1600px]">
